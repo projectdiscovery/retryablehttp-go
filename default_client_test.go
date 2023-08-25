97ec74245afe00f544d84fe5991224b0257d246e
Tarun Koyalwar <tarun@projectdiscovery.io>
2023-08-25 21:23:04 +0530

    add unit test for connection reuse

--- default_client_test.go
+++ default_client_test.go
@@ -1,8 +1,16 @@
 package retryablehttp_test
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httptrace"
+	"sync"
+	"sync/atomic"
 	"testing"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/projectdiscovery/retryablehttp-go"
 	"github.com/stretchr/testify/require"
 )
@@ -14,4 +22,44 @@ func Test_DefaultHttpClient(t *testing.T) {
 	resp, err := retryablehttp.DefaultHTTPClient.Get("https://scanme.sh")
 	require.Nil(t, err)
 	require.NotNil(t, resp)
+}
+
+func TestConnectionReuse(t *testing.T) {
+	opts := retryablehttp.DefaultOptionsSingle
+	client := retryablehttp.NewClient(opts)
+
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		fmt.Fprintf(w, "this is a test")
+	})
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	trace := &httptrace.ClientTrace{}
+	totalConns := &atomic.Uint32{}
+	trace.ConnectStart = func(network, addr string) {
+		_ = totalConns.Add(1)
+	}
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for i := 0; i < 20; i++ {
+				req, err := retryablehttp.NewRequest("GET", ts.URL, nil)
+				require.Nil(t, err)
+				req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+				resp, err := client.Do(req)
+				require.Nil(t, err)
+				io.Copy(io.Discard, resp.Body)
+				resp.Body.Close()
+			}
+		}()
+	}
+
+	wg.Wait()
+	require.Equal(t, totalConns.Load(), uint32(5))
 }
